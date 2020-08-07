b393dde4ea535489e9ebb699c13bcf41631227f3
Malek BEKRI <malek.bekri@eniso.u-sousse.tn>
2020-08-07 21:49:47 +0100

    ajouter des icônes au composant de contact

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,30 +1,39 @@
 <template>
   <div class="contact">
     <div class="mail" v-show="mail">
-      <i class="far fa-envelope"></i>
+      <mail :size="12" />
       {{mail}}
     </div>
     <div class="phone" v-show="phone">
-      <i class="fas fa-phone"></i>
+      <phone :size="12" />
       {{phone}}
     </div>
     <div class="adress" v-show="adress">
+      <adress :size="12" />
       {{adress}}
     </div>
   </div>
 </template>
 
 <script>
+import mail from 'vue-material-design-icons/EmailOpenMultipleOutline.vue';
+import phone from 'vue-material-design-icons/PhoneHangupOutline.vue';
+import adress from 'vue-material-design-icons/City.vue';
+
 export default {
   props: ['mail', 'phone', 'adress'],
   name: 'Contact',
+  components: {
+    mail,
+    phone,
+    adress,
+  },
 };
 
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped >
-@import url('/assets/all.min.css');
 @import url('https://fonts.googleapis.com/css2?family=Nunito+Sans&display=swap');
 .contact{
   color:#5a6268;
